109d4c08d10b821c99a5dc7f4a5c53862bb324cb
Freekeyboy <jjhutton81@gmail.com>
2019-08-08 12:41:24 -0700

    Cleaned up miner.py file to reflect instructor guided lecture.

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -5,6 +5,9 @@ import sys
 
 
 # Implement functionality to search for a proof 
+# Both functions below were moved from basic blockchain
+#Removed self since it's no longer a class function
+# Added some print statements
 def proof_of_work(last_proof):
     """
     Simple Proof of Work Algorithm
@@ -46,19 +49,33 @@ if __name__ == '__main__':
     coins_mined = 0
     # Run forever until interrupted
     while True:
-        # Get the last proof from the server and look for a new one
-        # r = requests.get(url = node + "/last_proof")
-        #DO not need to use r.json(), but its good to use
-       
+        # Get the last proof from the server a
 
+        # generate request with /last_proof
+        # Will probably need a get request
+       
+        # Look for next proof based on results from above request
         new_proof = proof_of_work(last_proof)
-        data = {'proof': new_proof}
+
+        #When next proof found, POST it to the server {"proof": new_proof}
+        
+        #The server will validate our results to make sure proof has not already been submitted
+        #If valid, it will create a new block and reward with a coin.
+
+        #Research POST in python
+        #Research requests and remamber we're sending data as JSON
+        
         # Assign our new_proof to 'proof'
         proof_data = {'proof': new_proof}
 
         # requests.post to /mine endpoint
         r = requests.post(url = node + '/mine', data = data)
+        data = r.json()
 
+        #If the server responds with 'New Block Forged, add one coin
         if r.message == 'New Block Forged':
             coins_mined += 1
             print('Coines mined: ', coins_mined)
+        print(data.get('message'))
+        
+        #Our infinite loop will repeate until user does keyboard interupt in the client terminal.
